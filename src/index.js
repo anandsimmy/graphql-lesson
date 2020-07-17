f5a47d9576f8aeb84a5d59a5f19e2762b5a8d70c
Anand Simmy <anandsimmy7@gmail.com>
2020-07-17 16:20:08 +0530

    integrated with graphql server using apollo

--- src/index.js
+++ src/index.js
@@ -3,19 +3,46 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloClient, gql } from 'apollo-boost'
 
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+const httpLink= createHttpLink({
+  uri: 'https://crwn-clothing.com/'
+})
+const cache= new InMemoryCache()
+
+const client= new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+client.query({
+  query: gql`
+  {
+    getCollectionsByTitle(title: "jackets"){
+        id,
+        title
+      }
+  }
+  `
+}).then(res=> console.log(res.data))
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+    </ApolloProvider>,
   document.getElementById('root')
 );
