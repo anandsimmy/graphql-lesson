638adfda2da5b98e8567f4bb315482030ed1c28f
Anand Simmy <anandsimmy7@gmail.com>
2020-07-26 18:10:15 +0530

    additemtocart mutation added

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from '../collection-item/collection-item-container';
 
 import './collection-preview.styles.scss';
 
